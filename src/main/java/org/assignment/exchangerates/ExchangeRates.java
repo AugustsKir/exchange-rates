ddaebc0332d8b03742bbe59be63dd2c8495baf97
AugustsKir <kirsisaugusts@gmail.com>
2022-12-15 00:40:07 +0200

    grouped rates in hashmap

--- src/main/java/org/assignment/exchangerates/ExchangeRates.java
+++ src/main/java/org/assignment/exchangerates/ExchangeRates.java
@@ -5,12 +5,11 @@ import io.javalin.http.Context;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.math.BigDecimal;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.sql.Connection;
-import java.sql.DriverManager;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashMap;
 
 public final class ExchangeRates {
     private static final String URL = "https://www.bank.lv/vk/ecb_rss.xml";
@@ -31,15 +30,20 @@ public final class ExchangeRates {
     public static void output(Context context) throws IOException {
         BufferedReader in = new BufferedReader(new InputStreamReader(address.openStream()));
         String line;
+        HashMap<String, BigDecimal> map = new HashMap<>();
         while ((line = in.readLine()) != null) {
             if (line.contains("<description>") && line.contains("AUD")) {
                 int start = line.indexOf("AUD");
                 int end = line.indexOf("]]><");
                 String temp = line.substring(start, end);
-                System.out.println(temp);
+                String[] arr = temp.split(" ");
+                for (int i = 0; i < arr.length ; i += 2) {
+                    map.put(arr[i], new BigDecimal(arr[i + 1]));
+                }
             }
         }
         in.close();
+        context.result(map.toString());
 
 
     }
